256902b3b1cf1c3624895881c105cb308d039068
VPetar <freeze.988@gmail.com>
2021-08-28 00:16:16 +0200

    Fix lint issue

--- src/components/globals/ButtonSpinner/index.vue
+++ src/components/globals/ButtonSpinner/index.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script lang="ts">
-import {  Vue } from "vue-class-component";
+import { Vue } from "vue-class-component";
 export default class Home extends Vue {
   loading = false;
 
